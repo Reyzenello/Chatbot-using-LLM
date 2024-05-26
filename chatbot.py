ed17c3c40b7aed2079709a4cfcceea257d0197da
RiccardoBruzzese <43668563+Reyzenello@users.noreply.github.com>
2024-05-26 12:02:15 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,19 +2,12 @@
 import streamlit as st
 
 from langchain_community.document_loaders import WebBaseLoader
-
 from langchain_community.vectorstores import Chroma
-
 from langchain_community import embeddings
-
 from langchain_community.llms import Ollama
-
 from langchain_core.runnables import RunnablePassthrough
-
 from langchain_core.output_parsers import StrOutputParser
-
 from langchain_core.prompts import ChatPromptTemplate
-
 from langchain.text_splitter import CharacterTextSplitter 
 
 
